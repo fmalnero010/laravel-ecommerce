68fc0696402f238cd593e25ad115cda0f2d0c9ad
Facundo Malnero <facundomalnero010@gmail.com>
2025-10-12 20:58:23 -0300

    chore: adds ide-helper

--- src/Users/Domain/Models/User.php
+++ src/Users/Domain/Models/User.php
@@ -4,21 +4,10 @@ declare(strict_types=1);
 
 namespace Src\Users\Domain\Models;
 
-use Carbon\CarbonInterface;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-/**
- * @property-read int $id
- * @property-read string $name
- * @property-read string $email
- * @property-read CarbonInterface|null $email_verified_at
- * @property-read string $password
- * @property-read string|null $remember_token
- * @property-read CarbonInterface $created_at
- * @property-read CarbonInterface $updated_at
- */
 final class User extends Authenticatable implements MustVerifyEmail
 {
     use Notifiable;
