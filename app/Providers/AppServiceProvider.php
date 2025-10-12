a83f4bf4fe01383f2e7dc78ca8e913ed89d64a1e
Facundo Malnero <facundomalnero010@gmail.com>
2025-10-12 20:15:56 -0300

    first commit

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Providers;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Validation\Rules\Password;
+
+final class AppServiceProvider extends ServiceProvider
+{
+    public function register(): void
+    {
+        //
+    }
+
+    public function boot(): void
+    {
+        $this->bootModelsDefaults();
+    }
+
+    private function bootModelsDefaults(): void
+    {
+        Model::unguard();
+    }
+}
